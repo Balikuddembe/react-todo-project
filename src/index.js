2757f5651b906c78bde2db8cd145bf3c0d837b93
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-02-23 22:40:49 +0300

    Fix review comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoContainer from './ClassBasedComponents/components/TodoContainer';
-// import Header from './ClassBasedComponents/components/Header';
-// import TodoItem from './ClassBasedComponents/components/TodoItem';
-// import TodosList from './ClassBasedComponents/components/TodosList';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Header /> */}
-    <TodoContainer />
-    {/* <TodoItem />
-    <TodosList /> */}
-    {/* <App /> */}
+    <Router basename="/">
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
 );
